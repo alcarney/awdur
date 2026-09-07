4f9b9d8aac90fd9b7aa336421d8d18b7b1113fef
Alex Carney <alcarneyme@gmail.com>
2026-09-07 10:21:18 +0100

    awdur: write an initial check in commit
    Fossil compresses most of its content, if you create an empty repo
    with `fossil init` and look in the `blob` table you will see a bunch
    of bytes.
    
    >>> import sqlite3
    >>> db = sqlite3.connect('empty.fossil')
    >>> row = db.execute('SELECT content from BLOB')
    >>> dat = next(row)[0]
    >>> dat
    b'\x00\x00\x00\xa3x\x9c\x1d\xc8K\n\x830\x10\x00\xd0}N1k!e\x12\xa3\xc9\xb8mOP\xec\xa6\xb8\xc9g\x82\xa2\x86\xa2\x16\xea\xedK\xbb|\xef\nS\x99\x8e\xc9/\xc3\xce\xeb\xeb8\x87=\x8e\x1cg9\x15q\x03\x8d\xba\x95H\x12\xdb^Qgl\xa7\xd5\xa5\xb1$\xee\x90\x8cJ.&r\x191h4L\x0e\x91\xc8q\xcc\xceh\xcb\xa2\x87*l\xbe\xc4\x11*8\xb6w\x99\x7f\xb3\x9f\xab\xfc\x03*\xf1\x00\xbf\xf0G<\xa1N\xa8T\xf2l\xb3r\xc6\xea\xbav\x9a\x1bG9s\xc06\xd8F|\x01.\t.\x1f'
    
    The record will include a 4 byte header
    
    >>> dat[:4]
    b'\x00\x00\x00\xa3'
    
    That is a *big endian* unsigned int denoting the uncompressed size of the blob.
    
    >>> import struct
    >>> struct.unpack('>I', dat[:4])
    (163,)
    
    Note that this header matches the size column on the same table.
    
    >>> import zlib
    >>> zlib.decompress(dat[4:])
    b'C initial\\sempty\\scheck-in\nD 2026-09-06T19:47:21.579\nR d41d8cd98f00b204e9800998ecf8427e\nT *branch * trunk\nT *sym-trunk *\nU alex\nZ 3d011dae7f184723382e589ffeb06b75\n'
    
    Once you remove the header, it's easy enough to decompress the record.
    
    >>> print(zlib.decompress(dat[4:]).decode())
    C initial\sempty\scheck-in
    D 2026-09-06T19:47:21.579
    R d41d8cd98f00b204e9800998ecf8427e
    T *branch * trunk
    T *sym-trunk *
    U alex
    Z 3d011dae7f184723382e589ffeb06b75
    
    See the fossil documentation for details on the record itself.
    https://fossil-scm.org/home/doc/trunk/www/fileformat.wiki

--- lib/awdur/awdur/project/fossil.py
+++ lib/awdur/awdur/project/fossil.py
@@ -1,9 +1,30 @@
+"""Export a project to a fossil source repo.
+
+.. seealso::
+
+   `src/schema.c <https://fossil-scm.org/home/file?name=src%2Fschema.c>`__
+      The code defining the structure of the database
+
+   `Fossil File Format <https://fossil-scm.org/home/doc/trunk/www/fileformat.wiki>`__
+      Defines the structure of each of the main db records, manifest, tickets, etc.
+
+   `Fossil is not Relational <https://fossil-scm.org/home/doc/trunk/www/fossil-is-not-relational.md>`__
+      Notes on the overall data model.
+"""
+
 from __future__ import annotations
 
+import hashlib
 import pathlib
 import sqlite3
+import struct
 import typing
+import zlib
 from datetime import datetime
+from datetime import timezone
+
+UTC = timezone.utc
+
 
 if typing.TYPE_CHECKING:
     from .project import Project
@@ -11,6 +32,14 @@ if typing.TYPE_CHECKING:
 
 
 SCHEMA = pathlib.Path(__file__).parent / "fossil_schema.sql"
+
+ASCII_SPACE = chr(0x20)
+ASCII_NL = chr(0x0A)
+ASCII_BSLASH = chr(0x5C)
+
+ESC_SPACE = chr(0x5C) + chr(0x73)
+ESC_NL = chr(0x5C) + chr(0x6E)
+ESC_BSLASH = 2 * ASCII_BSLASH
 
 
 class FossilExporter:
@@ -24,28 +53,244 @@ def init_db(output: pathlib.Path):
     db = sqlite3.connect(output.with_suffix(".fossil"))
     _ = db.executescript(SCHEMA.read_text())
 
-    now = int(datetime.now().timestamp())
+    # now = datetime.now(tz=UTC)
+    now = datetime.fromisoformat("2026-09-06T19:47:21.579")
+    mtime = int(now.timestamp())
+
     _ = db.executemany(
         "INSERT INTO config(name,value,mtime) VALUES (?,?,?)",
         [
-            ("aux-schema", "2015-01-24", now),
-            # TODO: generate a proper code, derived from project so it's stable...
-            ("project-code", "12", now),
+            ("aux-schema", "2015-01-24", mtime),
+            ("content-schema", "2", mtime),
+            ("hash-policy", "2", mtime),
+            # TODO: generate proper codes,
+            ("project-code", "12", mtime),
+            ("server-code", "34", mtime),
         ],
     )
 
     # Without some user accounts ``fossil ui`` will not show anything.
-    _ = db.executemany(
-        "INSERT INTO user(login,pw,cap,info,mtime,jx) VALUES (?,?,?,?,?,?)",
-        [
+    username = "alex"
+    users = {
+        u.login: u.insert(db)
+        for u in [
             # TODO: generate an admin account with default password
-            ("anonymous", "", "hz", "Anon", None, "{}"),
-            ("nobody", "", "gjorz", "Noobdy", None, "{}"),
-            ("developer", "", "ei", "Dev", None, "{}"),
-            ("reader", "", "kptw", "Reader", None, "{}"),
-        ],
+            User(username, pw="", cap="s", info=username, mtime=now),
+            User("anonymous", pw="", cap="hz", info="Anon", mtime=now),
+            User("nobody", pw="", cap="gjorz", info="Noobdy", mtime=now),
+            User("developer", pw="", cap="ei", info="Dev", mtime=now),
+            User("reader", pw="", cap="kptw", info="Reader", mtime=now),
+        ]
+    }
+    user = users[username]
+
+    # Write the blob containing the initial check-in
+    manifest = Manifest(comment="initial empty check-in", date=now, user=user.login)
+    manifest.add_tag("branch", "trunk")
+    manifest.add_tag("sym-trunk")
+
+    blob = Blob.create(manifest.build())
+    blob = blob.insert(db)
+
+    # Update rcvfrom
+    _ = db.execute(
+        "INSERT INTO rcvfrom(rcvid,uid,mtime) VALUES (?,?,julianday(?, 'unixepoch'))",
+        (1, user.uid, mtime),
     )
 
     db.commit()
-
     return db
+
+
+@typing.final
+class Manifest:
+    """Responsible for building the manifest entry for a check-in."""
+
+    def __init__(self, comment: str, date: datetime, user: str):
+        self.comment = comment
+        self.date = date
+        self.repo_checksum = hashlib.md5()
+        self.tags: list[tuple[str, str]] = []
+        self.user = user
+
+    def add_tag(self, name: str, value: str = ""):
+        self.tags.append((name, value))
+
+    def build(self) -> str:
+        """Close the manifest by appending the ``Z`` checksum card and return it."""
+        cards: list[str] = [
+            f"C {escape_text(self.comment)}",
+            f"D {format_date(self.date)}",
+        ]
+
+        # TODO: Include files...
+
+        cards.append(f"R {self.repo_checksum.hexdigest()}")
+
+        for name, value in self.tags:
+            # '*' refers to 'self' i.e. this manifest, see file format spec  for details.
+            cards.append(f"T *{name} * {value}".strip())
+
+        cards.append(f"U {self.user}")
+
+        record = "\n".join(cards) + "\n"
+        checksum = hashlib.md5(record.encode()).hexdigest()
+        l = record + f"Z {checksum}\n"
+        print(l)
+        return l
+
+
+@typing.final
+class Blob:
+    """Represents a record from the ``blob`` table."""
+
+    def __init__(
+        self, rid: int | None, rcvid: int, size: int, uuid: str, content: bytes
+    ):
+        self.rid = rid
+        self.rcvid = rcvid
+        self.size = size
+        self.uuid = uuid
+        self.content = content
+
+    def __repr__(self):
+        return f"Blob<{self.uuid}; {self.size} bytes>"
+
+    @classmethod
+    def fromdb(cls, rid: int, rcvid: int, size: int, uuid: str, content: bytes):
+        return cls(rid, rcvid, size, uuid, content)
+
+    def insert(self, db: sqlite3.Connection | sqlite3.Cursor):
+        """Insert this record into the db."""
+        cursor = db.execute(
+            "INSERT INTO blob(rcvid, size, uuid, content) VALUES (?,?,?,?) RETURNING *",
+            (self.rcvid, self.size, self.uuid, self.content),
+        )
+        return Blob.fromdb(*cursor.fetchone())
+
+    @classmethod
+    def create(cls, content: str):
+        """Create a new blob record."""
+        bcontent = content.encode()
+        uuid = hashlib.sha3_256(bcontent).hexdigest()
+
+        data = zlib.compress(bcontent)
+        size = len(bcontent)
+        header = struct.pack(">I", size)
+
+        # Not entirely sure what this should be... I have a feeling it's related
+        # to syncing, so *should* be ok to set to 1 for now.
+        rcvid = 1
+
+        return cls(None, rcvid, size, uuid, header + data)
+
+
+@typing.final
+class User:
+    """Represents a row from the ``user`` table."""
+
+    def __init__(
+        self,
+        login: str,
+        *,
+        uid: int | None = None,
+        pw: str | None = None,
+        cap: str | None,
+        cookie: str | None = None,
+        ipaddr: str | None = None,
+        cexpire: str | None = None,
+        info: str | None = None,
+        mtime: datetime | None = None,
+        photo: bytes | None = None,
+        jx: str = "",
+    ):
+        self.login = login
+        self.uid = uid
+        self.pw = pw
+        self.cap = cap
+        self.cookie = cookie
+        self.ipaddr = ipaddr
+        self.cexpire = cexpire
+        self.info = info
+        self.mtime = mtime
+        self.photo = photo
+        self.jx = jx
+
+    @classmethod
+    def fromdb(
+        cls,
+        uid: int,
+        login: str,
+        pw: str | None,
+        cap: str | None,
+        cookie: str | None,
+        ipaddr: str | None,
+        cexpire: str | None,
+        info: str | None,
+        mtime: str,
+        photo: bytes | None,
+        jx: str,
+    ):
+        return cls(
+            login,
+            uid=uid,
+            pw=pw,
+            cap=cap,
+            cookie=cookie,
+            ipaddr=ipaddr,
+            cexpire=cexpire,
+            info=info,
+            mtime=datetime.fromisoformat(mtime),
+            photo=photo,
+            jx=jx,
+        )
+
+    def insert(self, db: sqlite3.Connection | sqlite3.Cursor):
+        cursor = db.execute(
+            "INSERT INTO user(login,pw,cap,info,mtime) VALUES (?,?,?,?,?) "
+            "RETURNING uid,login,pw,cap,cookie,ipaddr,cexpire,info,"
+            "strftime('%FT%TZ', mtime, 'unixepoch'),photo,jx",
+            (
+                self.login,
+                self.pw,
+                self.cap,
+                self.info,
+                int(self.mtime.timestamp()) if self.mtime else None,
+            ),
+        )
+        return User.fromdb(*cursor.fetchone())
+
+
+def escape_text(text: str) -> str:
+    """Escape text for inclusion in a fossil record.
+
+    .. pull-quote::
+
+       The following escape sequences are applied to the text:
+       - A space (ASCII 0x20) is represented as "\\s" (ASCII 0x5C, 0x73).
+       - A newline (ASCII 0x0a) is "\\n" (ASCII 0x5C, x6E).
+       - A backslash (ASCII 0x5C) is represented as two backslashes "\\\\".
+
+       -- `C card <https://fossil-scm.org/home/doc/trunk/www/fileformat.wiki#manifest>`__
+    """
+    return (
+        text.replace(ASCII_BSLASH, ESC_BSLASH)
+        .replace(ASCII_SPACE, ESC_SPACE)
+        .replace(ASCII_NL, ESC_NL)
+    )
+
+
+def format_date(dt: datetime) -> str:
+    """Format a datetime for inclusion in a fossil record.
+
+    .. pull-quote::
+
+       The sole argument to the D card is a date-time stamp in the ISO8601 format.
+
+       The date and time should be in coordinated universal time (UTC).
+       The format one of:
+
+       - YYYY-MM-DDTHH:MM:SS
+       - YYYY-MM-DDTHH:MM:SS.SSS
+    """
+    return dt.isoformat("T", "milliseconds").replace("+00:00", "")
--- lib/awdur/awdur/project/fossil_schema.sql
+++ lib/awdur/awdur/project/fossil_schema.sql
@@ -1,4 +1,85 @@
--- Fossil database schema:
+-- Fossil database schema
+-- see: https://fossil-scm.org/home/file?&name=src%252Fschema.c
+
+CREATE TABLE blob(
+  rid INTEGER PRIMARY KEY,
+  rcvid INTEGER,
+  size INTEGER,
+  uuid TEXT UNIQUE NOT NULL,
+  content BLOB,
+  CHECK( length(uuid)>=40 AND rid>0 )
+);
+
+CREATE TABLE delta(
+  rid INTEGER PRIMARY KEY,
+  srcid INTEGER NOT NULL REFERENCES blob
+);
+CREATE INDEX delta_i1 ON delta(srcid);
+
+
+CREATE TABLE rcvfrom(
+  rcvid INTEGER PRIMARY KEY,
+  uid INTEGER REFERENCES user,
+  mtime DATETIME,
+  nonce TEXT UNIQUE,
+  ipaddr TEXT
+);
+
+
+CREATE TABLE config(
+  name TEXT PRIMARY KEY NOT NULL,
+  value CLOB,
+  mtime DATE,
+  CHECK( typeof(name)='text' AND length(name)>=1 )
+) WITHOUT ROWID;
+
+CREATE TABLE user(
+  uid INTEGER PRIMARY KEY,
+  login TEXT UNIQUE,
+  pw TEXT,
+  cap TEXT,
+  cookie TEXT,
+  ipaddr TEXT,
+  cexpire DATETIME,
+  info TEXT,
+  mtime DATE,
+  photo BLOB,
+  jx TEXT DEFAULT '{}'
+);
+
+CREATE TABLE shun(
+  uuid TEXT PRIMARY KEY,
+  mtime DATE,
+  scom TEXT
+) WITHOUT ROWID;
+
+
+CREATE TABLE private(rid INTEGER PRIMARY KEY);
+
+
+CREATE TABLE reportfmt(
+   rn INTEGER PRIMARY KEY,
+   owner TEXT,
+   title TEXT UNIQUE,
+   mtime DATE,
+   cols TEXT,
+   sqlcode TEXT,
+   jx TEXT DEFAULT '{}'
+);
+
+CREATE TABLE concealed(
+  hash TEXT PRIMARY KEY,
+  mtime DATE,
+  content TEXT
+) WITHOUT ROWID;
+
+-- The application ID helps the unix "file" command to identify the
+-- database as a fossil repository.
+PRAGMA application_id=252006673;
+
+
+-- Anything below this line is rebuilt with ``fossil rebuild`` and we
+-- shouldn't ever have to touch it.
 
 CREATE TABLE attachment(
   attachid INTEGER PRIMARY KEY,
@@ -19,39 +100,12 @@ CREATE TABLE backlink(
   UNIQUE(target, srctype, srcid)
 );
 
-CREATE TABLE blob(
-  rid INTEGER PRIMARY KEY,
-  rcvid INTEGER,
-  size INTEGER,
-  uuid TEXT UNIQUE NOT NULL,
-  content BLOB,
-  CHECK( length(uuid)>=40 AND rid>0 )
-);
-
 CREATE TABLE cherrypick(
   parentid INT,
   childid INT,
   isExclude BOOLEAN DEFAULT false,
   PRIMARY KEY(parentid, childid)
 ) WITHOUT ROWID;
-
-CREATE TABLE concealed(
-  hash TEXT PRIMARY KEY,
-  mtime DATE,
-  content TEXT
-) WITHOUT ROWID;
-
-CREATE TABLE config(
-  name TEXT PRIMARY KEY NOT NULL,
-  value CLOB,
-  mtime DATE,
-  CHECK( typeof(name)='text' AND length(name)>=1 )
-) WITHOUT ROWID;
-
-CREATE TABLE delta(
-  rid INTEGER PRIMARY KEY,
-  srcid INTEGER NOT NULL REFERENCES blob
-);
 
 CREATE TABLE event(
   type TEXT,
@@ -103,32 +157,6 @@ CREATE TABLE plink(
   baseid INTEGER REFERENCES blob,
   UNIQUE(pid, cid)
 );
-
-CREATE TABLE private(rid INTEGER PRIMARY KEY);
-
-CREATE TABLE rcvfrom(
-  rcvid INTEGER PRIMARY KEY,
-  uid INTEGER REFERENCES user,
-  mtime DATETIME,
-  nonce TEXT UNIQUE,
-  ipaddr TEXT
-);
-
-CREATE TABLE reportfmt(
-   rn INTEGER PRIMARY KEY,
-   owner TEXT,
-   title TEXT UNIQUE,
-   mtime DATE,
-   cols TEXT,
-   sqlcode TEXT,
-   jx TEXT DEFAULT '{}'
-);
-
-CREATE TABLE shun(
-  uuid TEXT PRIMARY KEY,
-  mtime DATE,
-  scom TEXT
-) WITHOUT ROWID;
 
 CREATE TABLE tag(
   tagid INTEGER PRIMARY KEY,
@@ -182,16 +210,13 @@ CREATE TABLE unsent(
   rid INTEGER PRIMARY KEY
 );
 
-CREATE TABLE user(
-  uid INTEGER PRIMARY KEY,
-  login TEXT UNIQUE,
-  pw TEXT,
-  cap TEXT,
-  cookie TEXT,
-  ipaddr TEXT,
-  cexpire DATETIME,
-  info TEXT,
-  mtime DATE,
-  photo BLOB,
-  jx TEXT DEFAULT '{}'
-);
+CREATE VIEW artifact(
+  rid,
+  rcvid,
+  size,
+  atype,
+  srcid,
+  hash,
+  content
+) AS SELECT blob.rid,rcvid,size,1,srcid,uuid,content
+FROM blob LEFT JOIN delta ON (blob.rid=delta.rid)
